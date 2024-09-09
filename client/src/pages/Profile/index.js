dea607e0016b7b850905203fad924383af8f2a40
nakiet <cybell888@gmail.com>
2024-09-09 09:52:40 +0700

    profile

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,33 +1,65 @@
 import React from 'react';
-import {Image} from "@nextui-org/react";
+import {Image, Button} from "@nextui-org/react";
+import { FaGithub } from "react-icons/fa";
+import { FaRegFilePdf } from "react-icons/fa6";
+import { SiPostman } from "react-icons/si";
 
 function ProfileCard() {
   return (
-      <div className="mx-auto grid grid-cols-5 bg-slate-200 bg-opacity-10 rounded-3xl w-full">
-          <Image 
-            src="/emotewater.webp" 
-            alt="Avatar" 
-            width="200" 
-            radius="full"
-            className="my-12 ml-44  col-span-1" 
-          />
+      <div className="mx-auto grid grid-cols-5 bg-slate-200 bg-opacity-30 rounded-3xl w-full">
+          <Image src="/emotewater.webp" alt="Avatar" width="200" radius="full" className="my-12 ml-44  col-span-1" />
 
-          <div className="col-span-4 mt-16 ml-44 flex items-center">
+          <div className="col-span-4 mt-24 ml-44 flex items-center">
               <div className="mb-auto">
-                  <span className="text-5xl font-bold text-white mb-3">Nguyễn Anh Kiệt</span>
-                  <p className='text-4xl font-bold text-white'>Mã sinh viên: B21DCCN471</p>
-                  <span>Số điện thoại: 0392514756</span>
+                  <span className="text-5xl font-bold text-white block mb-4">Nguyễn Anh Kiệt</span>
+                  <span className="text-3xl text-white block mb-4">Mã sinh viên: B21DCCN471</span>
+                  <span className="text-3xl text-white block">Số điện thoại: 0392514756</span>
+
+                  <div className="mt-8 grid grid-cols-3 gap-2">
+                    <div className=''>
+                        <Button 
+                            
+                            className="bg-slate-950 text-white text-2xl"
+                            radius="sm"
+                            size="lg"
+                            startContent={<FaGithub />}
+                            fullWidth
+                        >
+                            <a href="#">GitHub</a>
+                        </Button>
+                    </div>
+
+                    <div className=''>
+                        <Button 
+                            color="success"
+                            className="bg-red-400 text-white text-2xl"
+                            radius="sm"
+                            size="lg"
+                            startContent={<FaRegFilePdf />}
+                            fullWidth
+                        >
+                            <a href="#">PDF</a>
+                        </Button>
+                    </div>
+
+                    <div className=''>
+                        <Button 
+                            color="success"
+                            className="bg-orange-400 text-white text-2xl"
+                            radius="sm"
+                            size="lg"
+                            startContent={<SiPostman />}
+                            fullWidth
+                        >
+                            <a href="#">API Doc</a>
+                        </Button>
+                    </div>
+                    
+                    
+                  </div>
               </div>
 
-              {/* <div>
-                  <a href="#">GitHub</a>
-              </div>
-              <div>
-                  <a href="#">Download PDF</a>
-              </div>
-              <div>
-                  <a href="#">API Doc</a>
-              </div> */}
+              
           </div>
       </div>
   );
